422b3c2bc2a66cf4d0c8b0519a4fc8c9ba87b233
Manoj <manojgrg314@gmail.com>
2024-10-04 12:18:46 -0500

    make it into command line too using typer

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+import typer
 from dotenv import load_dotenv
 load_dotenv()
 import sys
 import os
+from pprint import pprint
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
+app = typer.Typer(invoke_without_command=True) # Initialize the typer cli with no command support
+
 from src.agent_graph import custom_graph
+
+@app.command()
+def defualt():
+
+    while (True):
+        question = input("What is your question? ")
+        if not question:
+            return
+        inputs = {"question": question}
+
+        for output in custom_graph.stream(inputs):
+            for key, value in output.items():
+                pprint(f"Finished running: {key}:")
+        print(value["generation"])
 
 if __name__ == "__main__":
 
-    from pprint import pprint
-    inputs = {"question": "Who is the president of fisk university?"}
-
-    for output in custom_graph.stream(inputs):
-        for key, value in output.items():
-            pprint(f"Finished running: {key}:")
-    print(value["generation"])
+    app()
+    
--- src/constant.py
+++ src/constant.py
@@ -2,12 +2,12 @@ app_name = "omeka-s-rag-bot"
 collection_name = "omeka-s-rag-chroma"
 local_llm = "llama3"
 omeka_s_urls = [
-    'https://omeka.org/s/docs/developer/api/',
-    'https://omeka.org/s/docs/developer/api/php_api/',
-    'https://omeka.org/s/docs/developer/api/rest_api/',
-    'https://omeka.org/s/docs/developer/api/representations/',
-    'https://omeka.org/s/docs/developer/api/api_reference/',
-    'https://omeka.org/s/docs/developer/api/rest_api_reference/'    
+    #'https://omeka.org/s/docs/developer/api/',
+    # 'https://omeka.org/s/docs/developer/api/php_api/',
+    # 'https://omeka.org/s/docs/developer/api/rest_api/',
+    # 'https://omeka.org/s/docs/developer/api/representations/',
+    # 'https://omeka.org/s/docs/developer/api/api_reference/',
+    # 'https://omeka.org/s/docs/developer/api/rest_api_reference/'    
 ]
 env_crawl_omeka_s = "CRAWL_OMEKA_S"
 env_tavily_api = "TAVILY_API_KEY"
